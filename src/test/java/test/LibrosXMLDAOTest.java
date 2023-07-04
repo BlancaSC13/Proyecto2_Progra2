dbde9a9d0aed0f5acf7fdb46540e74b86b594c55
BlancaSC13 <blanca.segura@ucr.ac.cr>
2023-07-04 16:14:57 -0600

    Revert "init"
    This reverts commit 59481189f60bc10ec865df4feba8d8872a8ec279.

--- src/test/java/test/LibrosXMLDAOTest.java
+++ src/test/java/test/LibrosXMLDAOTest.java
@@ -18,6 +18,11 @@ public class LibrosXMLDAOTest {
     public void probarXML() throws IOException, JDOMException {
         crear();
         librosXMLDAO = LibrosXMLDAO.abrirDocumento("libros.xml");
+        try {
+            System.out.println(librosXMLDAO.buscarTitulo("harry potter"));
+        } catch (DataConversionException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
@@ -33,7 +38,21 @@ public class LibrosXMLDAOTest {
     public void eliminarLibro() throws IOException, JDOMException {
         crear();
         librosXMLDAO.eliminarLibro(1);
-
+    }
+    @Test
+    public void buscarPorAutor () throws IOException, JDOMException {
+        crear();
+        System.out.println(librosXMLDAO.buscarAutor(2));
+    }
+    @Test
+    public void buscarPorTematica () throws IOException, JDOMException {
+        crear();
+        System.out.println(librosXMLDAO.buscarTematica("Ficción"));
+    }
+    @Test
+    public void buscarPorEditorial () throws IOException, JDOMException {
+        crear();
+        System.out.println(librosXMLDAO.buscarEditorial("Planeta"));
     }
     private void crear() throws IOException {
         librosXMLDAO = LibrosXMLDAO.crearDocumento("libros.xml");
